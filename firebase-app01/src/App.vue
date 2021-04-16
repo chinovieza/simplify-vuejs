e18d591b49d4586c3094caeb65f0c2c6f1945ff1
Chino Smitthikunanon <chinovieza@gmail.com>
2021-04-16 15:26:51 +0700

    First firebase (firestore) app

--- firebase-app01/src/App.vue
+++ firebase-app01/src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App with Firestore"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import firestore from './database/firebase'
+
+const studentRef = firestore.collection("students");
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created() {
+    studentRef.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        if (doc.data()) {
+          console.log(doc.id);
+          console.log(doc.data().name);
+          console.log(doc.data().age);
+        }
+      });
+    });
   }
 }
 </script>
