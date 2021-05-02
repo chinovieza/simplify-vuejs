0b855c3d74e84bed359fb03b5ce041bdc0ec6c96
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-02 09:49:35 +0700

    Prepare firebase

---
+++ firebase-app02/src/database/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.VUE_APP_FIREBASE_APPID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
