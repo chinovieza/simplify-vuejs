8febb533295b5b9baa65150158fec762f4fcf2c6
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-02 11:46:15 +0700

    Display user login profile

--- firebase-app02/src/App.vue
+++ firebase-app02/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <SignIn :user="user" />
+    <div v-if="user">
+      <hr/>
+      <div style="{margin:10+'px'}">
+        <div>uid : {{user.uid}} </div>
+        <div>name: {{user.displayName}} </div>
+        <div>email: {{user.email}} </div>
+      </div>
+      <div>
+        <img :src="user.photoURL" alt="user" width="80" height="80" />
+      </div>
+    </div>
   </div>
 </template>
 
