4f3b1e488f30d65563ada4f757153c94d0c3ac8d
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-02 22:00:16 +0700

    Store login user to the firestore

--- firebase-app02/src/SignIn.vue
+++ firebase-app02/src/SignIn.vue
@@ -5,20 +5,48 @@
     </div>
 </template>
 <script>
-import { auth, googleProvider } from "./database/firebase";
+import { firestore, auth, googleProvider } from "./database/firebase";
 export default {
     name: "SignIn",
     props: ["user"],
     methods: {
-        googleLoginHandler() {
-            auth
-                .signInWithPopup(googleProvider)
-                .then( user => {
-                    console.log(user);
-                })
-                .catch( err => {
-                    console.error(err);
-                });
+        // googleLoginHandler() {
+        //     auth
+        //         .signInWithPopup(googleProvider)
+        //         .then( user => {
+        //             console.log(user);
+        //         })
+        //         .catch( err => {
+        //             console.error(err);
+        //         });
+        // },
+        async googleLoginHandler() {
+            try {
+
+                const result = await auth.signInWithPopup(googleProvider);
+
+                if (result) {
+                    const userRef = firestore.collection("users").doc(result.user.uid);
+                    userRef.get().then( doc => {
+                        if (!doc.data()) {
+                            userRef.set({
+                                uid: result.user.uid,
+                                displayName:  result.user.displayName,
+                                photoURL: result.user.photoURL,
+                                email: result.user.email,
+                                created: new Date().valueOf(),
+                                role: "user"
+                            });
+                            console.log("Store new user to the firestore success");
+                        } else {
+                            console.log("Existing user, there not save to firestore");
+                        }
+                    });
+                }
+
+            } catch (err) {
+                console.log(err);
+            }
         },
         googleLogoutHandler() {
             auth
