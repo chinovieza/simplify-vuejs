9717c065a46ce2a07e2c9379a32ba0a0fbbc2642
Chino Smitthikunanon <chinovieza@gmail.com>
2021-05-03 23:31:32 +0700

    Create login by email/password / start by create App.vue

--- firebase-app03/src/App.vue
+++ firebase-app03/src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="row" v-if="loading">
+      <Loading />
+    </div>
+    <div class="row mt-1" v-else-if="user">
+      <div class="col-sm-10 mx-auto text-center">
+        <div class="display-4 mt-3">สวัสดี : {{ user.displayName }}</div>
+        <div v-show="imgLoading" class="mt-5 spinner-border text-primary text-center p-3" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+        <img v-show="!imgLoading" class="rounded float-center mt-2 border rounded" :src="user.photoURL" alt="user" width="200px" :style="{ maxHeight: 200 + 'px' }" @load="imageLoaded" />
+      </div>
+    </div>
+    <div class="row mt-1" v-if="user">
+      <div class="col-sm-10 mx-auto text-center">
+        <hr />
+        <button class="btn btn-info" @click="signOutHandler">Logout</button>
+      </div>
+    </div>
+    <div class="row mt-1" v-else>
+      <LoginForm />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { auth, firestore } from "./database/firebase";
+import LoginForm from "./LoginForm";
+import Loading from "./Loading";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      user: null,
+      loading: false,
+      imgLoading: true,
+      authUnsubscribe: null
+    };
+  },
   components: {
-    HelloWorld
+    Loading,
+    LoginForm
+  },
+  methods: {
+    signOutHandler() {
+      auth
+        .signOut()
+        .then(() => {
+          console.log("Logout OK");
+        })
+        .catch(err => {
+          console.log("Logout Not OK." + err);
+        });
+    },
+    setUserData(uid) {
+      firestore
+        .collection("users")
+        .doc(uid)
+        .onSnapshot(doc => {
+          if (doc.exists) {
+            const userObj = {
+              uid: doc.id,
+              displayName: doc.data().displayName,
+              email: doc.data().email,
+              photoURL: doc.data().photoURL
+            };
+            this.user = userObj;
+            this.$nextTick;
+          }
+        });
+    },
+    imageLoaded() {
+      this.imgLoading = false;
+    }
+  },
+  created() {
+    this.authUnsubscribe = auth.onAuthStateChanged(
+      user => {
+        if (user) {
+          this.setUserData(user.id)
+        } else {
+          this.user = null;
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  },
+  beforeDestroy() {
+    this.authUnsubscribe();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ firebase-app03/src/database/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.VUE_APP_FIREBASE_APPID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firestore = firebaseApp.firestore();
+export const auth = firebaseApp.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export default firebaseApp;
