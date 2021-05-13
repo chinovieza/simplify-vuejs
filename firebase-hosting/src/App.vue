bff3a7ea303e3e5b787423e1eda5dcb79e7818e4
Chino Smittikunanon <chino_smi@truecorp.co.th>
2021-05-13 17:35:17 +0700

    Prepare project

--- firebase-hosting/src/App.vue
+++ firebase-hosting/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Simplify Project by ChinoVieza powered by Vue.js App"/>
   </div>
 </template>
 
