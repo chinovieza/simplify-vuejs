199f54bd2dbd8251602c9b9a6a731252104a89f4
Chino Smittikunanon <chino_smi@truecorp.co.th>
2021-05-13 21:58:48 +0700

    Firebase cloud functions for firestore

--- firebase-cloudfunctions/functions/index.js
+++ firebase-cloudfunctions/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,3 +14,20 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.status(200).send("Hello Monkey D. Luffy World !!!");
 });
+
+exports.defaultName = functions.firestore
+    .document("users/{userId}")
+    .onWrite((change) => {
+        if (!change.after.exists) {
+            return null;
+        }
+        const { email, displayName, initialized } = change.after.data();
+        if (email && !initialized) {
+            return change.after.ref.update({
+                email: email,
+                displayName: !displayName ? "Anonymous" : displayName,
+                initialized: true
+            });
+        }
+        return null;
+    });
